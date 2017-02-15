8f6079ad4f6c69b8dd580c4101eb58cccca3bab2
dar-pin-gin <darkpinguin@darkpinguin.net>
2017-02-15 19:39:01 +0100

    firts upload

---
+++ unit1.pas
@@ -0,0 +1,176 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls, fphttpclient, Unit2, process;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Update_Label: TLabel;
+    Preamie: TButton;
+    sendmail: TButton;
+    rm: TButton;
+    Exit_Button: TButton;
+    Update_Button: TButton;
+    Edit1: TEdit;
+    preamie_in_euro: TEdit;
+    GroupBox1: TGroupBox;
+    Label1: TLabel;
+    RadioButton1: TRadioButton;
+    RadioButton2: TRadioButton;
+    RadioGroup1: TRadioGroup;
+    wq: TGroupBox;
+    Jahre: TLabel;
+    procedure Exit_ButtonClick(Sender: TObject);
+    procedure PreamieClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure rmClick(Sender: TObject);
+    procedure sendmailClick(Sender: TObject);
+    procedure Update_ButtonClick(Sender: TObject);
+    function is_Portable_APP(App: String):Boolean;
+  private
+    { private declarations }
+    const
+    Progame_Name = 'Preamienrechner';
+    version      = 0.0;
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  setup   :Boolean;
+  p       :Extended;
+  bz      :Extended;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.PreamieClick(Sender: TObject);
+begin
+  p:=0;
+  bz:= StrToFloat(Edit1.Text);
+  //ShowMessage(FloatToStr(bz));
+  if NOT(bz < 1)then
+  begin
+    p:=200;
+    if (bz >= 6)then
+    begin
+      p:= p + 80 +bz * 20;
+      if (RadioButton2.Checked)then
+      begin
+        p:= p + 50;
+      end;
+    end;
+  end;
+  preamie_in_euro.Text:= FloatToStr(p);
+  //Form2.search_for_Updates(Update_Label,version,Progame_Name);
+end;
+
+procedure TForm1.Exit_ButtonClick(Sender: TObject);
+begin
+  Form1.Close();
+end;
+
+
+procedure TForm1.FormCreate(Sender: TObject); //will be extracted to SASetup Programm
+var
+  inifile :TextFile;
+  inicode :string;
+  exe     :string;
+  uiwdiimw:Boolean;
+begin
+  setup:=False;
+  //exe:=ExtractFileName(Application.ExeName);
+
+  //if NOT(is_Portable_APP(exe))then
+  //begin
+    //AssignFile(inifile,Progame_Name+'.ini');
+    //if FileExists(Progame_Name+'.ini') then
+    //begin
+         //Reset(inifile);
+         //ReadLn(inifile,inicode);
+         //if(inicode = '5C6AA54174C00EF5')then
+         //begin
+              //CloseFile(inifile);
+              //setup:=False;
+         //end
+    //end
+    //else
+    //begin
+         //Rewrite(inifile);
+         //Write(inifile,'5C6AA54174C00EF5');
+         //CloseFile(inifile);
+         //setup:=True;
+    //end;
+  //end;
+
+end;
+
+procedure TForm1.rmClick(Sender: TObject);
+begin
+  Edit1.Clear();
+  preamie_in_euro.Clear();
+  RadioButton1.Checked:=True;
+  //Form2.search_for_Updates(Update_Label,version,Progame_Name);
+end;
+
+procedure TForm1.sendmailClick(Sender: TObject);
+var
+  thunderbird : TProcess;
+begin
+  //Form2.search_for_Updates(Update_Label,version,Progame_Name);
+  thunderbird := TProcess.Create(Nil);
+  if FileExistsUTF8('c:\\Program Files\Mozilla Thunderbird\thunderbird.exe') then
+  begin
+    thunderbird.CommandLine:='c:\\Program Files\Mozilla Thunderbird\thunderbird.exe -compose "to=name@exsample.com,subject=Preamie,body=Hallo Frau/Herr NAME Sie Erhalten eine Preamie von '+FloatToStr(p)+' Euro. Mit freundlichen Gruessen"';
+  end else if FileExistsUTF8('c:\\Program Files (x86)\Mozilla Thunderbird\thunderbird.exe')then
+  begin
+    thunderbird.CommandLine:='c:\\Program Files (x86)\Mozilla Thunderbird\thunderbird.exe -compose "to=name@exsample.com,subject=Preamie,body=Hallo Frau/Herr NAME Sie Erhalten eine Preamie von '+FloatToStr(p)+' Euro. Mit freundlichen Gruessen"';
+  end else if FileExistsUTF8('c:\\ProgramData\Microsoft\Windows\Start Menu\Programs\Mozilla Thunderbird\thunderbird.exe ')then
+  begin
+    thunderbird.CommandLine:='c:\\ProgramData\Microsoft\Windows\Start Menu\Programs\Mozilla Thunderbird\thunderbird.exe -compose "to=name@exsample.com,subject=Preamie,body=Hallo Frau/Herr NAME Sie Erhalten eine Preamie von '+FloatToStr(p)+' Euro. Mit freundlichen Gruessen"';
+  end else begin
+    ShowMessage('Mozilla Thunderbird only');
+    if FileExistsUTF8('c:\\Program Files\Internet Explorer\iexplore.exe') then
+  begin
+    thunderbird.CommandLine:='c:\\Program Files\Internet Explorer\iexplore.exe';
+  end else begin
+      ShowMessage('Your are broken because NO INTERNET EXPLORER / Edge (iexplore.exe) was found\n');
+  end;
+    Form1.Close;
+  end;
+  thunderbird.Execute;
+end;
+
+procedure TForm1.Update_ButtonClick(Sender: TObject);
+begin
+  Form2.ShowInTaskBar:=TShowInTaskbar.stDefault;
+  //Form2.search_for_Updates();
+  Form2.Show;
+end;
+
+function TForm1.is_Portable_APP(App:String):Boolean;
+begin
+  if(0 <pos('portable.exe',App))then
+     begin
+       Result:=True
+     end
+     else
+     begin
+       Result:=False;
+     end;
+end;
+
+end.
+
---
+++ unit2.pas
@@ -0,0 +1,144 @@
+unit Unit2;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls, CheckLst, ExtCtrls, fphttpclient,
+  strutils, convutils, varutils, XMLRead, DOM;
+
+type
+
+  { TForm2 }
+  TSarray = array of string;
+  TForm2 = class(TForm)
+
+    Exit_Button: TButton;
+    Update_Button: TButton;
+    Install: TButton;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    CheckBox3: TCheckBox;
+    CheckGroup1: TCheckGroup;
+    CheckListBox1: TCheckListBox;
+    procedure CheckBox3Change(Sender: TObject);
+    procedure CheckBox3Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure InstallClick(Sender: TObject);
+    procedure search_for_Updates();
+    function ProInstall(Programname :String):Boolean;
+    function search_for_Updates(Updatelabe : TLabel; version: Extended;Programname :String):Boolean;
+  private
+    { private declarations }
+  public
+    { public declarations }
+
+    var
+       Programs      : string;
+       newUpdate     : Boolean;
+
+
+  end;
+
+var
+  Form2         : TForm2;
+  Doc           : TXMLDocument;
+  node          : TDOMNode;
+  Stringarray : array of string;
+  gaeste: array[1..10] of string;
+  Programms: array [1..20] of TDOMNode;
+  nodes      :TDOMNodeList;
+  S: TStringStream;
+implementation
+
+{$R *.lfm}
+
+{ TForm2 }
+
+
+procedure TForm2.CheckBox3Click(Sender: TObject);
+begin
+  CheckBox3.Checked:=False;
+  ShowMessage('Beta Only');
+end;
+
+procedure TForm2.CheckBox3Change(Sender: TObject);
+begin
+
+end;
+
+
+
+
+procedure TForm2.FormCreate(Sender: TObject);
+var
+  inifile :TextFile;
+begin
+  //Programs:= TFPCustomHTTPClient.SimpleGet('http://darkpinguin.net/DV/Programms.xml.update');
+  //AssignFile(inifile,'.aini');
+  //Rewrite(inifile);
+  //Write(inifile,Programs);
+  //CloseFile(inifile);
+  //ShowMessage(Application.ExeName);
+  //Doc := load
+  //ReadXMLFile(Doc,'http://darkpinguin.net/DV/Programs.xml.update');
+  //nodes:=Doc.DocumentElement.FindNode('programms').GetChildNodes;
+  //ShowMessage(Programms[0].Attributes.Item[0].ToString);
+  //Stringarray:= TForm2.Split(Programs,'|');
+  //ShowMessage();                                                    ><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  ////CheckListBox1.Items.AddStrings();
+  //CheckListBox1.Items.Add('Preamienrechner');
+  //CheckListBox1.Items.Add('SETUP');
+  //CheckListBox1.Items.Add('Taschenrehner(keine Updates mehr)');
+end;
+
+procedure TForm2.InstallClick(Sender: TObject);
+var
+  Http : TFPCustomHTTPClient;
+begin
+  ProInstall('Preamienrechner');
+  Http := TFPCustomHTTPClient.Create(nil);
+  Http.Get('http://79.209.87.4/DV/releases/Preamienrechner/version.dat','version.dat');
+
+
+
+end;
+
+procedure TForm2.search_for_Updates();
+var
+  i : Integer;
+  len : String;
+  proname : String;
+begin
+  ShowMessage(TFPCustomHTTPClient.SimpleGet('http://darkpinguin.net/DV/len.data'));
+  len := TFPCustomHTTPClient.SimpleGet('http://darkpinguin.net/DV/len.data');
+  for i:= 0 to StrtoInt(len[1]) do begin
+    proname := TFPCustomHTTPClient.SimpleGet('http://darkpinguin.net/DV/'+InttoStr(i)+'/name.dat');
+    CheckListBox1.Items.Add(copy(proname,0,length(proname)-1));
+  end;
+end;
+function TForm2.search_for_Updates(Updatelabe : TLabel; version: Extended;Programname :String) : Boolean;
+var
+  netversion:Extended;
+  tmp       :string;
+begin
+  Result:=False;
+  tmp:= TFPCustomHTTPClient.SimpleGet('http://darkpinguin.net/DV/releases/'+Programname+'/version.dat');
+  netversion:=StrToFloat(copy(tmp,1,length(tmp)-1));
+  if netversion > version then
+  begin
+    Updatelabe.Visible:=True;
+    Result:=True
+  end;
+end;
+
+//function TForm2.ProInstall(Programname :string) :Boolean;
+//begin
+  //TFPCustomHTTPClient.Get('http://darkpinguin.net/DV/releases/'+Programname+'/'+Programname+'.exe',Programname+'new.exe');
+
+//end;
+
+
+end.
+
